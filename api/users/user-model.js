96f0b4c4fe0d3228397808049b60523298dcf8e7
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-12-09 19:54:40 +0100

    wip

--- api/users/user-model.js
+++ api/users/user-model.js
@@ -3,6 +3,14 @@ const db = require("../../data/db-config.js");
 // user-model
 module.exports = {
   getUserPosts() {
-    return db('')
+    // select
+    //   p.id,
+    //   u.username,
+    //   p.contents
+    // from users u
+    // join posts p
+    //   on p.user_id = u.id
+    // where u.id = 1;
+    return db('users u')
   }
 }
