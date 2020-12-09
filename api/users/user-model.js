dec6f889383ad8177bbcfc2db58a810768ab0c30
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-12-09 19:58:33 +0100

    wip

--- api/users/user-model.js
+++ api/users/user-model.js
@@ -11,8 +11,8 @@ module.exports = {
     // join posts p
     //   on p.user_id = u.id
     // where u.id = 1;
-    return db('users u')
-      .join('posts p', 'p.user_id', 'u.id')
+    return db('users as u')
+      .join('posts as p', 'p.user_id', 'u.id')
       .select('p.id', 'u.username', 'p.contents')
       .where('u.id', id);
   }
